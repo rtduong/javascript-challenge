a2547dd9a29a41f780ff0895cd45b9fc360777fe
rtduong@uw.edu <rtduong@uw.edu>
2014-11-07 14:06:32 -0800

    Added state and other occupation
    finalllly.

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,28 @@
-/*
-    Signup Form Script
-    This script will load the state select list and validate the form before submission
-*/
+
+"use strict";
+
+function onReady() {
+	var selectState = document.getElementsByName("state");
+	for (var i = 0; i < usStates.length; i++) {
+		var state = document.createElement("option");
+		state.text = usStates[i].name;
+		state.abbrev = usStates[i].code;
+		selectState[0].appendChild(state);
+	}
+    
+	function otherOccupation() { 
+        var occupation = document.getElementById("occupation");
+        var selection = document.getElementsByName("occupationOther");
+		var input = document.getElementById("occupation").value;
+		if(input == "other") {
+			selection[0].style.display = "inline";
+		} else {
+			selection[0].style.display = "none";
+		}
+	}
+	occupation.addEventListener('change', otherOccupation);
+}
+
+document.addEventListener('DOMContentLoaded', onReady);
+
+
